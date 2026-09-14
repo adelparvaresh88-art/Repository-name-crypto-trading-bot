cffb086a99056e7f73f9ae5d6f0d5d0215f70f4e
adelparvaresh88-art <adel.parvaresh88@gmail.com>
2026-09-14 18:56:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("ATI Crypto Trading Bot is ready!")
+from datetime import datetime
+
+print("================================")
+print("      ATI CRYPTO BOT")
+print("================================")
+print("Bot status: ONLINE")
+print("Mode: PAPER / TEST")
+print("Trading: DISABLED")
+print("Time:", datetime.now())
+print("================================")
+print("Bot is running successfully.")
